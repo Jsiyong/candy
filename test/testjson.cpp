712560ffd6805a94ef14886127668615ce5560c3
jiangsiyong <jiangsiyong@winsing.net>
2021-03-23 00:07:05 +0800

    修改Json测试

--- test/testjson.cpp
+++ test/testjson.cpp
@@ -33,19 +33,17 @@ int main() {
     std::string str = "{ \"你好\":{ \"I\\\"NT\":122,\"NULL\":null , \"FALSE\": false, \"TRUE\": true},\"k1\":\"v1\",\"k2\" : \"v2\",\"k3\" : {\"j1\":\"j2\"}, \"k4\":[\"1\",{\"k2\":\"k1\"},\"3\",\"4\", { } ,[],{\"1\":1},[1],[-200],[1.222222]] }";
     //std::string str = "[\"1\",{\"k2\":\"k1\"},\"3\",\"4\",[]]";
     //std::string str = "[1]";
-    std::vector<char> json(str.begin(), str.end());
 
-    JsonValue root = JsonParser::parse(json);
+    JsonValue root = JsonParser::toJsonValue(str);
 
-    jsonStr = JsonParser::parse(root);
+    jsonStr = JsonParser::toJson(root);
     jsonStr.push_back('\0');
     std::cout << jsonStr.data() << std::endl;
 
     //字符串转义
     jsonObj.insert("1234\"", JsonValue());
-    jsonStr = JsonParser::parse(jsonObj);
-    jsonStr.push_back('\0');
-    std::cout << jsonStr.data() << std::endl;
+    jsonStr = JsonParser::toJson(jsonObj);
+    std::cout << jsonStr.c_str() << std::endl;
 
 
     ABC abc;
@@ -59,15 +57,13 @@ int main() {
 
 
     jsonObj = Serializable<ABC>::serialize(abc);
-    jsonStr = JsonParser::parse(jsonObj);
-    jsonStr.push_back('\0');
-    std::cout << jsonStr.data() << std::endl;
+    jsonStr = JsonParser::toJson(jsonObj);
+    std::cout << jsonStr.c_str() << std::endl;
 
     ABC t = Serializable<ABC>::deserialize(jsonObj);
     jsonObj = Serializable<ABC>::serialize(t);
-    jsonStr = JsonParser::parse(jsonObj);
-    jsonStr.push_back('\0');
-    std::cout << jsonStr.data() << std::endl;
+    jsonStr = JsonParser::toJson(jsonObj);
+    std::cout << jsonStr.c_str() << std::endl;
 
     return 0;
 }
